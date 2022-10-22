2eafdb2c601de2c6387bd382b6ab7056a2b4656c
KM <username@users.noreply.github.com>
2022-10-22 16:19:11 +0200

    Third commit

--- main.js
+++ main.js
@@ -1,14 +1,363 @@
-let eventTarget = document.getElementById('black');
-let blackback1 = function() {
-    eventTarget.style.backgroundColor('white');
-}
-let blackback2 = function() {
-    eventTarget.style.color('black');
-}
-let blackback3 = function() {
-    eventTarget.style.boxShadow('6px 6px black');
-}
-eventTarget.addEventListener('click', function() {
-    eventTarget.style.backgroundColor('white');
+// laptop js
+let eventTarget = document.getElementById('black1');
+eventTarget.addEventListener('mouseover' , function() {
+    eventTarget.style.backgroundColor = 'white';
 });
-eventTarget.onclick = blackback1;
+eventTarget.addEventListener('mouseover' , function() {
+    eventTarget.style.color = 'black';
+});
+eventTarget.addEventListener('mouseover' , function() {
+    eventTarget.style.boxShadow = '6px 6px black';
+});
+eventTarget.addEventListener('mouseout' , function() {
+    eventTarget.style.backgroundColor = 'black';
+});
+eventTarget.addEventListener('mouseout' , function() {
+    eventTarget.style.color = 'white';
+});
+eventTarget.addEventListener('mouseout' , function() {
+    eventTarget.style.boxShadow = '6px 6px white';
+});
+let eventTarget1 = document.getElementById('black2');
+eventTarget1.addEventListener('mouseover' , function() {
+    eventTarget1.style.backgroundColor = 'white';
+});
+eventTarget1.addEventListener('mouseover' , function() {
+    eventTarget1.style.color = 'black';
+});
+eventTarget1.addEventListener('mouseover' , function() {
+    eventTarget1.style.boxShadow = '6px 6px black';
+});
+eventTarget1.addEventListener('mouseout' , function() {
+    eventTarget1.style.backgroundColor = 'black';
+});
+eventTarget1.addEventListener('mouseout' , function() {
+    eventTarget1.style.color = 'white';
+});
+eventTarget1.addEventListener('mouseout' , function() {
+    eventTarget1.style.boxShadow = '6px 6px white';
+});
+let eventTarget2 = document.getElementById('black3');
+eventTarget2.addEventListener('mouseover' , function() {
+    eventTarget2.style.backgroundColor = 'white';
+});
+eventTarget2.addEventListener('mouseover' , function() {
+    eventTarget2.style.color = 'black';
+});
+eventTarget2.addEventListener('mouseover' , function() {
+    eventTarget2.style.boxShadow = '6px 6px black';
+});
+eventTarget2.addEventListener('mouseout' , function() {
+    eventTarget2.style.backgroundColor = 'black';
+});
+eventTarget2.addEventListener('mouseout' , function() {
+    eventTarget2.style.color = 'white';
+});
+eventTarget2.addEventListener('mouseout' , function() {
+    eventTarget2.style.boxShadow = '6px 6px white';
+});
+let eventTarget3 = document.getElementById('black4');
+eventTarget3.addEventListener('mouseover' , function() {
+    eventTarget3.style.backgroundColor = 'white';
+});
+eventTarget3.addEventListener('mouseover' , function() {
+    eventTarget3.style.color = 'black';
+});
+eventTarget3.addEventListener('mouseover' , function() {
+    eventTarget3.style.boxShadow = '6px 6px black';
+});
+eventTarget3.addEventListener('mouseout' , function() {
+    eventTarget3.style.backgroundColor = 'black';
+});
+eventTarget3.addEventListener('mouseout' , function() {
+    eventTarget3.style.color = 'white';
+});
+eventTarget3.addEventListener('mouseout' , function() {
+    eventTarget3.style.boxShadow = '6px 6px white';
+});
+let eventTarget4 = document.getElementById('white1');
+eventTarget4.addEventListener('mouseover' , function() {
+    eventTarget4.style.backgroundColor = 'black';
+});
+eventTarget4.addEventListener('mouseover' , function() {
+    eventTarget4.style.color = 'white';
+});
+eventTarget4.addEventListener('mouseover' , function() {
+    eventTarget4.style.boxShadow = '6px 6px white';
+});
+eventTarget4.addEventListener('mouseout' , function() {
+    eventTarget4.style.backgroundColor = 'white';
+});
+eventTarget4.addEventListener('mouseout' , function() {
+    eventTarget4.style.color = 'black';
+});
+eventTarget4.addEventListener('mouseout' , function() {
+    eventTarget4.style.boxShadow = '6px 6px black';
+});
+let eventTarget5 = document.getElementById('white2');
+eventTarget5.addEventListener('mouseover' , function() {
+    eventTarget5.style.backgroundColor = 'black';
+});
+eventTarget5.addEventListener('mouseover' , function() {
+    eventTarget5.style.color = 'white';
+});
+eventTarget5.addEventListener('mouseover' , function() {
+    eventTarget5.style.boxShadow = '6px 6px white';
+});
+eventTarget5.addEventListener('mouseout' , function() {
+    eventTarget5.style.backgroundColor = 'white';
+});
+eventTarget5.addEventListener('mouseout' , function() {
+    eventTarget5.style.color = 'black';
+});
+eventTarget5.addEventListener('mouseout' , function() {
+    eventTarget5.style.boxShadow = '6px 6px black';
+});
+let eventTarget6 = document.getElementById('white3');
+eventTarget6.addEventListener('mouseover' , function() {
+    eventTarget6.style.backgroundColor = 'black';
+});
+eventTarget6.addEventListener('mouseover' , function() {
+    eventTarget6.style.color = 'white';
+});
+eventTarget6.addEventListener('mouseover' , function() {
+    eventTarget6.style.boxShadow = '6px 6px white';
+});
+eventTarget6.addEventListener('mouseout' , function() {
+    eventTarget6.style.backgroundColor = 'white';
+});
+eventTarget6.addEventListener('mouseout' , function() {
+    eventTarget6.style.color = 'black';
+});
+eventTarget6.addEventListener('mouseout' , function() {
+    eventTarget6.style.boxShadow = '6px 6px black';
+});
+// for phone users
+// 1
+eventTarget.addEventListener('touchstart' , function() {
+    eventTarget.style.backgroundColor = 'white';
+});
+eventTarget.addEventListener('touchstart' , function() {
+    eventTarget.style.color = 'black';
+});
+eventTarget.addEventListener('touchstart' , function() {
+    eventTarget.style.boxShadow = '6px 6px black';
+});
+eventTarget.addEventListener('touchmove' , function() {
+    eventTarget.style.backgroundColor = 'white';
+});
+eventTarget.addEventListener('touchmove' , function() {
+    eventTarget.style.color = 'black';
+});
+eventTarget.addEventListener('touchmove' , function() {
+    eventTarget.style.boxShadow = '6px 6px black';
+});
+eventTarget.addEventListener('touchend' , function() {
+    eventTarget.style.backgroundColor = 'black';
+});
+eventTarget.addEventListener('touchend' , function() {
+    eventTarget.style.color = 'white';
+});
+eventTarget.addEventListener('touchend' , function() {
+    eventTarget.style.boxShadow = '6px 6px white';
+});
+// 2
+eventTarget1.addEventListener('touchstart' , function() {
+    eventTarget1.style.backgroundColor = 'white';
+});
+eventTarget1.addEventListener('touchstart' , function() {
+    eventTarget1.style.color = 'black';
+});
+eventTarget1.addEventListener('touchstart' , function() {
+    eventTarget1.style.boxShadow = '6px 6px black';
+});
+eventTarget1.addEventListener('touchmove' , function() {
+    eventTarget1.style.backgroundColor = 'white';
+});
+eventTarget1.addEventListener('touchmove' , function() {
+    eventTarget1.style.color = 'black';
+});
+eventTarget1.addEventListener('touchmove' , function() {
+    eventTarget1.style.boxShadow = '6px 6px black';
+});
+eventTarget1.addEventListener('touchend' , function() {
+    eventTarget1.style.backgroundColor = 'black';
+});
+eventTarget1.addEventListener('touchend' , function() {
+    eventTarget1.style.color = 'white';
+});
+eventTarget1.addEventListener('touchend' , function() {
+    eventTarget1.style.boxShadow = '6px 6px white';
+});
+// 3
+eventTarget2.addEventListener('touchstart' , function() {
+    eventTarget2.style.backgroundColor = 'white';
+});
+eventTarget2.addEventListener('touchstart' , function() {
+    eventTarget2.style.color = 'black';
+});
+eventTarget2.addEventListener('touchstart' , function() {
+    eventTarget2.style.boxShadow = '6px 6px black';
+});
+eventTarget2.addEventListener('touchmove' , function() {
+    eventTarget2.style.backgroundColor = 'white';
+});
+eventTarget2.addEventListener('touchmove' , function() {
+    eventTarget2.style.color = 'black';
+});
+eventTarget2.addEventListener('touchmove' , function() {
+    eventTarget2.style.boxShadow = '6px 6px black';
+});
+eventTarget2.addEventListener('touchend' , function() {
+    eventTarget2.style.backgroundColor = 'black';
+});
+eventTarget2.addEventListener('touchend' , function() {
+    eventTarget2.style.color = 'white';
+});
+eventTarget2.addEventListener('touchend' , function() {
+    eventTarget2.style.boxShadow = '6px 6px white';
+});
+// 4
+eventTarget3.addEventListener('touchstart' , function() {
+    eventTarget3.style.backgroundColor = 'white';
+});
+eventTarget3.addEventListener('touchstart' , function() {
+    eventTarget3.style.color = 'black';
+});
+eventTarget3.addEventListener('touchstart' , function() {
+    eventTarget3.style.boxShadow = '6px 6px black';
+});
+eventTarget3.addEventListener('touchmove' , function() {
+    eventTarget3.style.backgroundColor = 'white';
+});
+eventTarget3.addEventListener('touchmove' , function() {
+    eventTarget3.style.color = 'black';
+});
+eventTarget3.addEventListener('touchmove' , function() {
+    eventTarget3.style.boxShadow = '6px 6px black';
+});
+eventTarget3.addEventListener('touchend' , function() {
+    eventTarget3.style.backgroundColor = 'black';
+});
+eventTarget3.addEventListener('touchend' , function() {
+    eventTarget3.style.color = 'white';
+});
+eventTarget3.addEventListener('touchend' , function() {
+    eventTarget3.style.boxShadow = '6px 6px white';
+});
+// 5
+eventTarget4.addEventListener('touchstart' , function() {
+    eventTarget4.style.backgroundColor = 'black';
+});
+eventTarget4.addEventListener('touchstart' , function() {
+    eventTarget4.style.color = 'white';
+});
+eventTarget4.addEventListener('touchstart' , function() {
+    eventTarget4.style.boxShadow = '6px 6px white';
+});
+eventTarget4.addEventListener('touchmove' , function() {
+    eventTarget4.style.backgroundColor = 'black';
+});
+eventTarget4.addEventListener('touchmove' , function() {
+    eventTarget4.style.color = 'white';
+});
+eventTarget4.addEventListener('touchmove' , function() {
+    eventTarget4.style.boxShadow = '6px 6px white';
+});
+eventTarget4.addEventListener('touchend' , function() {
+    eventTarget4.style.backgroundColor = 'white';
+});
+eventTarget4.addEventListener('touchend' , function() {
+    eventTarget4.style.color = 'black';
+});
+eventTarget4.addEventListener('touchend' , function() {
+    eventTarget4.style.boxShadow = '6px 6px black';
+});
+// 6
+eventTarget5.addEventListener('touchstart' , function() {
+    eventTarget5.style.backgroundColor = 'black';
+});
+eventTarget5.addEventListener('touchstart' , function() {
+    eventTarget5.style.color = 'white';
+});
+eventTarget5.addEventListener('touchstart' , function() {
+    eventTarget5.style.boxShadow = '6px 6px white';
+});
+eventTarget5.addEventListener('touchmove' , function() {
+    eventTarget5.style.backgroundColor = 'black';
+});
+eventTarget5.addEventListener('touchmove' , function() {
+    eventTarget5.style.color = 'white';
+});
+eventTarget5.addEventListener('touchmove' , function() {
+    eventTarget5.style.boxShadow = '6px 6px white';
+});
+eventTarget5.addEventListener('touchend' , function() {
+    eventTarget5.style.backgroundColor = 'white';
+});
+eventTarget5.addEventListener('touchend' , function() {
+    eventTarget5.style.color = 'black';
+});
+eventTarget5.addEventListener('touchend' , function() {
+    eventTarget5.style.boxShadow = '6px 6px black';
+});
+// 7 
+eventTarget6.addEventListener('touchstart' , function() {
+    eventTarget6.style.backgroundColor = 'black';
+});
+eventTarget6.addEventListener('touchstart' , function() {
+    eventTarget6.style.color = 'white';
+});
+eventTarget6.addEventListener('touchstart' , function() {
+    eventTarget6.style.boxShadow = '6px 6px white';
+});
+eventTarget6.addEventListener('touchmove' , function() {
+    eventTarget6.style.backgroundColor = 'black';
+});
+eventTarget6.addEventListener('touchmove' , function() {
+    eventTarget6.style.color = 'white';
+});
+eventTarget6.addEventListener('touchmove' , function() {
+    eventTarget6.style.boxShadow = '6px 6px white';
+});
+eventTarget6.addEventListener('touchend' , function() {
+    eventTarget6.style.backgroundColor = 'white';
+});
+eventTarget6.addEventListener('touchend' , function() {
+    eventTarget6.style.color = 'black';
+});
+eventTarget6.addEventListener('touchend' , function() {
+    eventTarget6.style.boxShadow = '6px 6px black';
+});
+// javascript for images (phone) 
+let imageTarget = document.getElementById('ora');
+let imageTarget1 = document.getElementById('sei');
+let imageTarget2 = document.getElementById('tea');
+let imageTarget3 = document.getElementById('style');
+let imageTarget4 = document.getElementById('cheat');
+let imageTarget5 = document.getElementById('mixed');
+let imageTarget6 = document.getElementById('guess');
+imageTarget.addEventListener('click', function() {
+    imageTarget.style.boxShadow = "6px 6px #037c6e"; 
+});
+imageTarget1.addEventListener('click', function() {
+    imageTarget1.style.boxShadow = "6px 6px #037c6e"; 
+});
+imageTarget2.addEventListener('click', function() {
+    imageTarget2.style.boxShadow = "6px 6px #037c6e"; 
+});
+imageTarget3.addEventListener('click', function() {
+    imageTarget3.style.boxShadow = "6px 6px #037c6e"; 
+});
+imageTarget4.addEventListener('click', function() {
+    imageTarget4.style.boxShadow = "6px 6px #037c6e"; 
+});
+imageTarget5.addEventListener('click', function() {
+    imageTarget5.style.boxShadow = "6px 6px #037c6e"; 
+});
+imageTarget6.addEventListener('click', function() {
+    imageTarget6.style.boxShadow = "6px 6px #037c6e"; 
+});
+
+
+
